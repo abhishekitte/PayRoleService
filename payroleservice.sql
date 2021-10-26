56a87c24ce3ed9acd4de620da1188b84e3234891
ABHI <abhishekitte11@gmail.com>
2021-10-26 15:37:06 +0530

    [ABHISHEK] Added a code to insert operation

--- payroleservice.sql
+++ payroleservice.sql
@@ -5,10 +5,13 @@ use payroleservice;
 
 ---------UC2-----------
 ---Create table for DataBase---
-CREATE TABLE employee_payrole(
+CREATE TABLE employee_payroll(
 	Id int Identity(1,1) PRIMARY KEY,
 	--Identity is a method--
 	Name varchar (200),
 	Salary float,
 	StartDate date
 	);
+
+	--------UC3 insert operation--------
+INSERT INTO employee_payroll VALUES('Abhishek','12345','2016/03/01') ,('Viahan','13335','2018/01/05'),('Virat','525245','2019/03/01');

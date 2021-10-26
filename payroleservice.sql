e92355b87bf15b51af430941e51ca6f2154cce34
ABHI <abhishekitte11@gmail.com>
2021-10-26 15:27:45 +0530

    [ABHISHEK] Added a code to create table

--- payroleservice.sql
+++ payroleservice.sql
@@ -2,3 +2,13 @@
 ---Create DataBase---
 Create database payroleservice;
 use payroleservice;
+
+---------UC2-----------
+---Create table for DataBase---
+CREATE TABLE employee_payrole(
+	Id int Identity(1,1) PRIMARY KEY,
+	--Identity is a method--
+	Name varchar (200),
+	Salary float,
+	StartDate date
+	);
